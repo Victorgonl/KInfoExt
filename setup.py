64a7300dfb10ecd12b12d9359336321e069e449c
Victorgonl <victorgonl@outlook.com>
2025-03-22 22:22:49 -0300

    Fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author_email="victorgonl@outlook.com",
     url="https://github.com/Victorgonl/kinfoex/",
     classifiers=[],
-    requires=["datasets==3.4.1",
+    packages=["datasets==3.4.1",
               "evaluate==0.4.3",
               "numpy==2.2.4",
               "optuna==4.2.1",
@@ -20,6 +20,5 @@ setup(
               "Pillow==11.1.0",
               "py-cpuinfo==9.0.0",
               "torch==2.6.0",
-              "transformers==4.50.0"
-]
+              "transformers==4.50.0"]
 )
